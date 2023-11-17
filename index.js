c0095c51d3ae152b0536d3fc40c629cbc01f036e
nabeel4557 <143259191+nabeel4557@users.noreply.github.com>
2023-11-17 09:45:55 +0530

    drop_down

--- index.js
+++ index.js
@@ -8,3 +8,71 @@ for(i = 0;i < x.length;i++) {
         this.style.background="black";
     })
 }
+let y = document.querySelectorAll("section#speaker div.container div.item div.bottom p")
+console.log(y)
+for(i = 0;i < x.length;i++) {
+    x[i].addEventListener("click",function(){
+        for(j=0;j<x.length;j++){
+            x[j].style.color="black";
+        }
+        this.style.color="white";
+    })
+}
+
+// let activate = document.querySelectorAll(".item")
+// activate.forEach(function (item) {
+//     item.addEventListener("click",function(){
+    
+//     let hide = document.querySelector("ul.hide")
+//     hide.style.display="block"
+
+// })
+
+// })
+
+
+let head = document.querySelectorAll(".item")
+head.forEach(function (items){
+    items.addEventListener("click",function(){
+        head.forEach(function (item){
+            item.classList.remove("active")
+
+        });
+
+        items.classList.add("active");
+
+        let clickedItem = items.getAttribute("data-id");
+        console.log(clickedItem)
+
+        let tabitems = document.querySelectorAll("ul.hide")
+        tabitems.forEach(function (tabitem){
+            tabitem.classList.remove("active");
+
+        });
+        document.getElementById(clickedItem).classList.add("active");
+
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function(){
+//     $('div.up').slick({
+//         dots: true,
+//         infinite: true,
+//         speed: 300,
+//         slidesToShow: 1,
+//         adaptiveHeight: true
+
+//       });
+//     })
