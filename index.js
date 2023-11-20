def81ed800134cbd9a2698889b12b13cd56b8088
nabeel4557 <143259191+nabeel4557@users.noreply.github.com>
2023-11-20 12:07:28 +0530

    slider

--- index.js
+++ index.js
@@ -36,26 +36,20 @@ head.forEach(function (items){
     items.addEventListener("click",function(){
         head.forEach(function (item){
             item.classList.remove("active")
-
         });
-
         items.classList.add("active");
-
         let clickedItem = items.getAttribute("data-id");
         console.log(clickedItem)
 
         let tabitems = document.querySelectorAll("ul.hide")
         tabitems.forEach(function (tabitem){
             tabitem.classList.remove("active");
-
         });
         document.getElementById(clickedItem).classList.add("active");
+    });
+});
 
-    })
-})
-
-
-
+// footer end'''
 
 
 
@@ -63,16 +57,46 @@ head.forEach(function (items){
 
 
 
+/////////////////////////////////
+// SLIDER 
+/////////////////////////////////
+const slideIndex = {};
 
+function initializeSlides(containerId) {
+    slideIndex[containerId] = 1;
+    showSlides(slideIndex[containerId], containerId);
+}
 
+// Next/previous controls for a specific container
+function plusSlides(n, containerId) {
+    showSlides((slideIndex[containerId] += n), containerId);
+}
 
-// $(document).ready(function(){
-//     $('div.up').slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         adaptiveHeight: true
+// Function to show slides for a specific container
+function showSlides(n, containerId) {
+    let i;
+    let slides = document.querySelectorAll(`#${containerId} .mySlides`);
 
-//       });
-//     })
+    // If the index goes beyond the last slide, reset to the first slide
+    if (n > slides.length) {
+        slideIndex[containerId] = 1;
+    }
+
+    // If the index goes below the first slide, set to the last slide
+    if (n < 1) {
+        slideIndex[containerId] = slides.length;
+    }
+
+    // Hide all slides
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    // Display the current slide
+    slides[slideIndex[containerId] - 1].style.display = "block";
+}
+
+// Initialize slides for each container
+initializeSlides("slider1");
+initializeSlides("slider2");
+initializeSlides("slider3");
